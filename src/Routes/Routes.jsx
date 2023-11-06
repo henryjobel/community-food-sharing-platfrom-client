cdbca9679838c6bcff5679e0b9b5970e51e44d31
henryjobel <jhobelsaha@gmail.com>
2023-11-06 11:28:44 +0600

    router setup and creatd nav links

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../Layouts/Root";
 import Home from "../Pages/Home/Home";
+import AvailableFood from "../Pages/Available_Food/AvailableFood";
+import AddFood from "../Pages/AddFood/AddFood";
+import ManageFood from "../Pages/Manage Food/ManageFood";
+import FoodRequest from "../Pages/FoodRequest/FoodRequest";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+
 
 
 const router = createBrowserRouter([
@@ -11,6 +18,30 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element:<Home></Home>
+            },
+            {
+                path: '/availablefood',
+                element:<AvailableFood></AvailableFood>
+            },
+            {
+                path: '/addfood',
+                element:<AddFood></AddFood>
+            },
+            {
+                path: '/managefood',
+                element:<ManageFood></ManageFood>
+            },
+            {
+                path: '/foodrequest',
+                element:<FoodRequest></FoodRequest>
+            },
+            {
+                path: '/login',
+                element:<Login></Login>
+            },
+            {
+                path: '/register',
+                element:<Register></Register>
             }
         ]
 
